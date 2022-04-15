787d5e7eb7e42b2871e147ef8b1876e66f34ec1f
ibash <ibash@LAPTOP-QKEAKK97>
2022-04-15 07:03:30 +0300

    Add project files.

---
+++ addressbook_tests_autoit/appmanager/HelperBase.cs
@@ -0,0 +1,16 @@
+﻿using AutoItX3Lib;
+namespace addressbook_tests_autoit
+{
+    public class HelperBase
+    {
+        protected AutoItX3 aux;
+        protected string WINTITLE;
+        protected ApplicationManager manager;
+        public HelperBase(ApplicationManager manager)
+        {
+            WINTITLE = ApplicationManager.WINTITLE;
+            this.manager = manager;
+            this.aux = manager.Aux;
+        }
+    }
+}
---
+++ addressbook_tests_autoit/tests/GroupRemovalTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace addressbook_tests_autoit
+{
+    [TestFixture]
+    public class GroupRemovalTests : TestBase
+    {
+        [Test]
+        public void TestGroupRemoval()
+        {
+            List<GroupData> oldGroups = app.Groups.GetGroupList();
+            if(oldGroups.Count == 1)
+            {
+                app.Groups.Add(new GroupData()
+                {
+                    Name = "GroupTest1"
+                });
+                
+                oldGroups = app.Groups.GetGroupList();
+            }
+            app.Groups.Remove(0);
+            List<GroupData> newGroups = app.Groups.GetGroupList();
+            oldGroups.RemoveAt(0);
+            oldGroups.Sort();
+            newGroups.Sort();
+            Assert.AreEqual(oldGroups, newGroups);
+
+        }
+    }
+}
